e85cecccf1d5d277c6ce611dc86a438dc1222675
melissa <146359791+lissa146@users.noreply.github.com>
2024-04-16 10:41:59 -0600

    Add files via upload

---
+++ fireball.py
@@ -0,0 +1,43 @@
+import pygame
+import math
+LEFT = 0
+RIGHT = 1
+UP = 2
+DOWN = 3
+SPACE = 4
+W = 5
+
+class fireball:
+    def __init__(self):
+        self.xpos = -10
+        self.ypos = -10
+        self.isAlive = False
+        self.direction = RIGHT
+        
+    def shoot(self, x, y, dir):
+        self.xpos = x + 20
+        self.ypos = y + 20
+        self.isAlive = True
+        self.direction = dir
+        
+    def move(self):
+        if self.direction == RIGHT:
+            self.xpos+=20
+        elif self.direction == LEFT:
+            self.xpos-=20
+        if self.direction == DOWN:
+            self.ypos+=20
+        elif self.direction == UP:
+            self.ypos-=20
+            
+    def draw(self, screen):
+        pygame.draw.circle(screen, (250, 0, 0), (self.xpos, self.ypos), 10)
+        pygame.draw.circle(screen, (250, 250, 0), (self.xpos, self.ypos), 5)
+        
+    def collide(self, x, y):
+        #distance formula for cucle collsion
+        if math.sqrt((self.xpos - x) ** 2 + (self.ypos - y) ** 2) < 25:
+            print("colison")
+            return True
+        else:
+            return False
